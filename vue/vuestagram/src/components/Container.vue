611b414cbd35f7c12c9350b3ae9926e192db401c
simon <zerojsh00@gmail.com>
2022-12-29 20:08:47 +0900

    [ADD] First Commit of Vuestagram

---
+++ vue/vuestagram/src/components/Container.vue
@@ -0,0 +1,20 @@
+<template>
+  <div v-for="(post, i) in postData" :key="i">
+    <Post v-bind:post="postData[i]" />
+  </div>
+</template>
+
+<script>
+import PostCmp from "./Post.vue";
+export default {
+  name: "ContainerCmp",
+  components: {
+    Post: PostCmp,
+  },
+  props: {
+    postData: Array,
+  },
+};
+</script>
+
+<style></style>
